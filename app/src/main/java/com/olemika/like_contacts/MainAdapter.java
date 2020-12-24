1bdbe9f21d32a1c0d22b8961ba099806c85d726b
Olesya Kuznetsova <73638532+olemika@users.noreply.github.com>
2020-12-24 21:17:57 +0300

    first commit

---
+++ app/src/main/java/com/olemika/like_contacts/MainAdapter.java
@@ -0,0 +1,22 @@
+package com.olemika.like_contacts;
+
+import android.view.ViewGroup;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainAdapter extends RecyclerView.Adapter {
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder( ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder( RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
